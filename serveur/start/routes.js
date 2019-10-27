636e7375cb1d1696642627f03bfb4c5570ae4df3
Cyrille MBIA <cyrille@itdreamreal.com>
2019-10-27 15:50:00 +0100

    Crud complet mis en place sans UI avec adonisJs + Ajout des tâches pour les projets

--- serveur/start/routes.js
+++ serveur/start/routes.js
@@ -25,5 +25,11 @@ Route.group(() => {
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
   Route.patch('projects/:id', 'ProjectController.update').middleware('auth');
 
+  Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth');
+  Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth');
+
+  Route.delete('tasks/:id', 'TaskController.destroy').middleware('auth');
+  Route.patch('tasks/:id', 'TaskController.update').middleware('auth');
+
 })
     .prefix('api');
